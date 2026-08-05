e79acc57cbc6743d7bb0f5154e1c8b9da33c51f2
YashVerma-0024 <yashverma29082008@gmail.com>
2026-08-05 10:52:49 +0530

    Adding file_types.py

---
+++ config/file_types.py
@@ -0,0 +1,30 @@
+EXTENSION_TO_CATEGORY = {
+    # Images
+    ".jpg" : "Images",
+    ".png" : "Images",
+    ".jpeg" : "Images",
+    ".gif" : "Images",
+
+    # Documents
+    ".doc" : "Documents",
+    ".docx" : "Documents",
+    ".pdf" : "Documents",
+    ".txt" : "Documents",
+    ".xlsx" : "Documents",
+
+    # Audio
+    ".mp3" : "Audio",
+    ".wav" : "Audio",
+    ".m4a" : "Audio",
+
+    # Videos
+    ".mp4" : "Videos",
+    ".m4v" : "Videos",
+    ".mkv" : "Videos",
+
+    # Archives
+    ".zip" : "Archives",
+    ".rar" : "Archives",
+    ".7z" : "Archives",
+    ".tar" : "Archives"
+}
