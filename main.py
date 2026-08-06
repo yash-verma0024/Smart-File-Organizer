8f29a2ee1100b59b57d6cb53bc62a8cd5b8ea388
YashVerma-0024 <yashverma29082008@gmail.com>
2026-08-06 13:42:41 +0530

    Adding main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Welcome back Sir...")
+    print("Smart-File-Organizer initializing...")
+    print('''
+System activated...
+Version : 1.1
+''')
+    
+if __name__ == "__main__":
+    main()
