acb41db2d06aef231dfda96d296c79d0f3b00ef5
YashVerma-0024 <yashverma29082008@gmail.com>
2026-08-06 22:11:47 +0530

    Adding metadata.py

---
+++ organizer/metadata.py
@@ -0,0 +1,35 @@
+from organizer.path_manager import create_path
+from organizer.validator import is_file
+from pathlib import Path
+from datetime import datetime
+
+def extract_metadata(path: str | Path) -> dict:
+    """
+    Extract metadata from a file.
+
+    """
+    path = create_path(path)
+
+    # raising error if file not exist
+    if not is_file(path):
+        raise FileNotFoundError(f"{path.name} is not a valid file")
+
+    file_stat = path.stat()
+
+    metadata = {
+        "name" : path.name,
+        "stem" : path.stem,
+        "extension" : path.suffix.lower(),
+        "parent" : str(path.parent),
+        "path" : str(path.resolve()),
+        "size" : file_stat.st_size,
+        "created_at": datetime.fromtimestamp(file_stat.st_ctime).isoformat(sep=" ", timespec="seconds"),
+        "modified_at": datetime.fromtimestamp(file_stat.st_mtime).isoformat(sep=" ", timespec="seconds"),
+    }
+
+    return metadata
+
+# Temporary testing
+if __name__ == "__main__":
+    path = r"C:\Users\verma\OneDrive\Pictures\Github\Project\Smart-File-Organizer\organizer\metadata.py"
+    print(extract_metadata(path))
