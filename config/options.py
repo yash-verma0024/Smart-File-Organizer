bd945f72e014f302834bb5aac3acc9f7d2850520
YashVerma-0024 <yashverma29082008@gmail.com>
2026-08-21 10:09:14 +0530

    Adding options.py

---
+++ config/options.py
@@ -0,0 +1,22 @@
+CORE_OPTION = ("""
+        1. Organize files in folder
+        2. Check Database
+        3. Synchronize Database
+""")
+
+DATABASE_FOLDER = ("""
+        1. See All records
+        2. Search For specific files
+        3. Update File path
+""")
+
+DATABASE_SEARCH = ("""
+        1. Search by id 
+        2. Search by name
+        3. Search by extension
+""")
+
+DATABASE_SORT_SEARCH = ("""
+        1. Search in Ascending order
+        2. Search in Descending order
+""")
